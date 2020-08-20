27667ddd72d5642443f738b98b9b4a0b03daf368
LordCracken <vladislav.yakimovskiy@mail.ru>
2020-08-21 02:46:53 +0300

    refact: refact jQuery code to Vue.js code; fix: fixed deletePage.php script

--- app/api/deleteHtmlPage.php
+++
@@ -1,8 +0,0 @@
-<?php
-$fileName = "../../" . $_POST["name"] . ".html";
-
-if (file_exists($newFile)) {
-  header("HTTP/1.0 400 Bad Request");
-} else {
-  unlink($fileName);
-}
--- app/src/main.js
+++ app/src/main.js
@@ -1,20 +1,53 @@
-const $ = require('jquery');
+// const $ = require('jquery');
 
-const getPageList = () => {
-  $('h2').remove();
-  $.get('./api', data => {
-    data.forEach(file => {
-      $('body').append(`<h2>${file}</h2>`)
-    });
-  }, 'JSON');
-};
+// const getPageList = () => {
+//   $('h2').remove();
+//   $.get('./api', data => {
+//     data.forEach(file => {
+//       $('body').append(`<h2>${file}</h2>`)
+//     });
+//   }, 'JSON');
+// };
 
-getPageList();
+// getPageList();
 
-$('#create-button').click(() => new Promise((resolve) => resolve($.post('./api/createNewHtmlPage.php', { name: $('input').val() })))
-  .then(getPageList)
-  .catch(() => alert('Такая страница уже существует!')));
+// $('#create-button').click(() => new Promise((resolve) => resolve($.post('./api/createNewHtmlPage.php', { name: $('input').val() })))
+//   .then(getPageList)
+//   .catch(() => alert('Такая страница уже существует!')));
 
-$('#delete-button').click(() => new Promise((resolve) => resolve($.post('./api/deleteHtmlPage.php', { name: $('input').val() })))
-  .then(getPageList)
-  .catch(() => alert('Такой страницы не существует!')));
+// $('#delete-button').click(() => new Promise((resolve) => resolve($.post('./api/deleteHtmlPage.php', { name: $('input').val() })))
+//   .then(getPageList)
+//   .catch(() => alert('Такой страницы не существует!')));
+
+import Vue from 'vue';
+import axios from 'axios';
+
+new Vue({
+  el: '#app',
+  data: {
+    pageList: [],
+    newPageName: ''
+  },
+  methods: {
+    createPage() {
+      axios
+        .post('./api/createNewHtmlPage.php', { name: this.newPageName })
+        .then(this.updatePageList)
+    },
+
+    updatePageList() {
+      axios
+        .get('./api/')
+        .then(response => this.pageList = response.data)
+    },
+
+    deletePage(page) {
+      axios
+        .post('./api/deletePage.php', { name: page })
+        .then(this.updatePageList)
+    }
+  },
+  created() {
+    this.updatePageList();
+  }
+})
